2a2df3714797a273598e7be1f3f791d24529e59a
tony-vu <tonyvu05@gmail.com>
2015-11-02 20:47:58 -0500

    Fix: Success callback for requirejs should pass undefine

--- src/ocLazyLoad.loaders.requireJSLoader.js
+++ src/ocLazyLoad.loaders.requireJSLoader.js
@@ -12,7 +12,7 @@
              * because the user can overwrite jsLoader and it will probably not use promises :(
              */
             $delegate.jsLoader = function (paths, callback, params) {
-                require(paths, callback, callback, params);
+                require(paths, callback.bind(null, undefined), callback, params);
             };
             $delegate.jsLoader.requirejs = true;
 
