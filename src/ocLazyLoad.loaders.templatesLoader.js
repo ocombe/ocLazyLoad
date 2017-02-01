c92bd2d8a2b5362888d246e71c10192d47394920
Alexander Eder <alexander.eder@farmnw070.local>
2017-02-01 14:45:43 +0100

    feat: angular 1.6 compatibility

--- src/ocLazyLoad.loaders.templatesLoader.js
+++ src/ocLazyLoad.loaders.templatesLoader.js
@@ -18,7 +18,8 @@
                 angular.forEach(paths, url => {
                     var deferred = $q.defer();
                     promises.push(deferred.promise);
-                    $http.get(url, params).success(data => {
+                    $http.get(url, params).then(response => {
+                        let data = response.data;
                         if(angular.isString(data) && data.length > 0) {
                             angular.forEach(angular.element(data), node => {
                                 if(node.nodeName === 'SCRIPT' && node.type === 'text/ng-template') {
@@ -30,8 +31,8 @@
                             filesCache.put(url, true);
                         }
                         deferred.resolve();
-                    }).error(function(err) {
-                        deferred.reject(new Error(`Unable to load template file "${ url }": ${ err }`));
+                    }).catch(response => {
+                        deferred.reject(new Error(`Unable to load template file "${ url }": ${ response.data }`));
                     });
                 });
                 return $q.all(promises).then(() => {
