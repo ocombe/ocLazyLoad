0b785e60778c94a8ecadb46824c94e14554024db
Brent Dearth <brent.dearth@gmail.com>
2018-04-26 15:05:14 -0600

    build and minify

--- dist/ocLazyLoad.require.js
+++ dist/ocLazyLoad.require.js
@@ -1,6 +1,6 @@
 /**
  * oclazyload - Load modules on demand (lazy load) with angularJS
- * @version v1.0.10
+ * @version v1.1.0
  * @link https://github.com/ocombe/ocLazyLoad
  * @license MIT
  * @author Olivier Combe <olivier.combe@gmail.com>
@@ -904,22 +904,26 @@
                     if (!uaCssChecked) {
                         var ua = $window.navigator.userAgent.toLowerCase();
 
-                        if (ua.indexOf('phantomjs/1.9') > -1) {
-                            // PhantomJS ~1.9
-                            useCssLoadPatch = true;
-                        } else if (/iP(hone|od|ad)/.test($window.navigator.platform)) {
-                            // iOS < 6
-                            var v = $window.navigator.appVersion.match(/OS (\d+)_(\d+)_?(\d+)?/);
-                            var iOSVersion = parseFloat([parseInt(v[1], 10), parseInt(v[2], 10), parseInt(v[3] || 0, 10)].join('.'));
-                            useCssLoadPatch = iOSVersion < 6;
-                        } else if (ua.indexOf('android') > -1) {
-                            // Android < 4.4
-                            var androidVersion = parseFloat(ua.slice(ua.indexOf('android') + 8));
-                            useCssLoadPatch = androidVersion < 4.4;
-                        } else if (ua.indexOf('safari') > -1) {
-                            // Safari < 6
-                            var versionMatch = ua.match(/version\/([\.\d]+)/i);
-                            useCssLoadPatch = versionMatch && versionMatch[1] && parseFloat(versionMatch[1]) < 6;
+                        try {
+                            if (ua.indexOf('phantomjs/1.9') > -1) {
+                                // PhantomJS ~1.9
+                                useCssLoadPatch = true;
+                            } else if (/iP(hone|od|ad)/.test($window.navigator.platform)) {
+                                // iOS < 6
+                                var v = $window.navigator.appVersion.match(/OS (\d+)_(\d+)_?(\d+)?/);
+                                var iOSVersion = parseFloat([parseInt(v[1], 10), parseInt(v[2], 10), parseInt(v[3] || 0, 10)].join('.'));
+                                useCssLoadPatch = iOSVersion < 6;
+                            } else if (ua.indexOf('android') > -1) {
+                                // Android < 4.4
+                                var androidVersion = parseFloat(ua.slice(ua.indexOf('android') + 8));
+                                useCssLoadPatch = androidVersion < 4.4;
+                            } else if (ua.indexOf('safari') > -1) {
+                                // Safari < 6
+                                var versionMatch = ua.match(/version\/([\.\d]+)/i);
+                                useCssLoadPatch = versionMatch && versionMatch[1] && parseFloat(versionMatch[1]) < 6;
+                            }
+                        } catch (e) {
+                            useCssLoadPatch = false;
                         }
                     }
 
@@ -1283,64 +1287,64 @@
 })(angular);
 // Array.indexOf polyfill for IE8
 if (!Array.prototype.indexOf) {
-        Array.prototype.indexOf = function (searchElement, fromIndex) {
-                var k;
+    Array.prototype.indexOf = function (searchElement, fromIndex) {
+        var k;
 
-                // 1. Let O be the result of calling ToObject passing
-                //    the this value as the argument.
-                if (this == null) {
-                        throw new TypeError('"this" is null or not defined');
-                }
+        // 1. Let O be the result of calling ToObject passing
+        //    the this value as the argument.
+        if (this == null) {
+            throw new TypeError('"this" is null or not defined');
+        }
 
-                var O = Object(this);
+        var O = Object(this);
 
-                // 2. Let lenValue be the result of calling the Get
-                //    internal method of O with the argument "length".
-                // 3. Let len be ToUint32(lenValue).
-                var len = O.length >>> 0;
+        // 2. Let lenValue be the result of calling the Get
+        //    internal method of O with the argument "length".
+        // 3. Let len be ToUint32(lenValue).
+        var len = O.length >>> 0;
 
-                // 4. If len is 0, return -1.
-                if (len === 0) {
-                        return -1;
-                }
+        // 4. If len is 0, return -1.
+        if (len === 0) {
+            return -1;
+        }
 
-                // 5. If argument fromIndex was passed let n be
-                //    ToInteger(fromIndex); else let n be 0.
-                var n = +fromIndex || 0;
+        // 5. If argument fromIndex was passed let n be
+        //    ToInteger(fromIndex); else let n be 0.
+        var n = +fromIndex || 0;
 
-                if (Math.abs(n) === Infinity) {
-                        n = 0;
-                }
+        if (Math.abs(n) === Infinity) {
+            n = 0;
+        }
 
-                // 6. If n >= len, return -1.
-                if (n >= len) {
-                        return -1;
-                }
+        // 6. If n >= len, return -1.
+        if (n >= len) {
+            return -1;
+        }
 
-                // 7. If n >= 0, then Let k be n.
-                // 8. Else, n<0, Let k be len - abs(n).
-                //    If k is less than 0, then let k be 0.
-                k = Math.max(n >= 0 ? n : len - Math.abs(n), 0);
+        // 7. If n >= 0, then Let k be n.
+        // 8. Else, n<0, Let k be len - abs(n).
+        //    If k is less than 0, then let k be 0.
+        k = Math.max(n >= 0 ? n : len - Math.abs(n), 0);
 
-                // 9. Repeat, while k < len
-                while (k < len) {
-                        // a. Let Pk be ToString(k).
-                        //   This is implicit for LHS operands of the in operator
-                        // b. Let kPresent be the result of calling the
-                        //    HasProperty internal method of O with argument Pk.
-                        //   This step can be combined with c
-                        // c. If kPresent is true, then
-                        //    i.  Let elementK be the result of calling the Get
-                        //        internal method of O with the argument ToString(k).
-                        //   ii.  Let same be the result of applying the
-                        //        Strict Equality Comparison Algorithm to
-                        //        searchElement and elementK.
-                        //  iii.  If same is true, return k.
-                        if (k in O && O[k] === searchElement) {
-                                return k;
-                        }
-                        k++;
-                }
-                return -1;
-        };
+        // 9. Repeat, while k < len
+        while (k < len) {
+            // a. Let Pk be ToString(k).
+            //   This is implicit for LHS operands of the in operator
+            // b. Let kPresent be the result of calling the
+            //    HasProperty internal method of O with argument Pk.
+            //   This step can be combined with c
+            // c. If kPresent is true, then
+            //    i.  Let elementK be the result of calling the Get
+            //        internal method of O with the argument ToString(k).
+            //   ii.  Let same be the result of applying the
+            //        Strict Equality Comparison Algorithm to
+            //        searchElement and elementK.
+            //  iii.  If same is true, return k.
+            if (k in O && O[k] === searchElement) {
+                return k;
+            }
+            k++;
+        }
+        return -1;
+    };
 }
